5884a2de346a3d4afd8a3de2f67057e5386a6225
noornee <noornee.011@gmail.com>
2022-11-03 19:39:25 +0100

    http server

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
+
+	port := os.Getenv("PORT")
+
+	mux := http.NewServeMux()
+
+	fmt.Printf("Server running on port %s", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatal(err)
+	}
 
 }
