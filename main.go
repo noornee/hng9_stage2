3a61bb003a40d84b3fed674982db26eada2ee04a
noornee <noornee.011@gmail.com>
2022-11-03 20:47:43 +0100

    added the arithmeticHandler to the mux

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	fmt.Printf("Server running on port %s", port)
+	mux.HandleFunc("/", arithmeticHandler)
+
+	fmt.Printf("Server running on port %s\n", port)
 	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatal(err)
 	}
