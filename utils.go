a2d0d059ccb818d6b28f25185e1f7459062d2e02
noornee <noornee.011@gmail.com>
2022-11-05 09:07:25 +0100

    made the algo handle cases of when the values of x and y is added to the operation_types

--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -13,6 +14,28 @@ func check(data Question) (int, string) {
 		"addition":       {"add", "addition", "plus", "sum"},
 		"subtraction":    {"subtract", "subtraction", "minus"},
 		"multiplication": {"multiply", "multiplication", "times"},
+	}
+
+	// checks if the values of X and Y is nil
+	// if the OperationType contains the values of X and Y,
+	// loop through it and reassign them to X and Y
+	if data.X == 0 && data.Y == 0 {
+		arr := strings.Split(data.OperationType, " ")
+		var num []int
+
+		for _, word := range arr {
+			val, err := strconv.Atoi(word)
+			if err != nil {
+				continue
+			}
+			num = append(num, val)
+		}
+
+		if len(num) == 2 {
+			data.X = num[0]
+			data.Y = num[1]
+		}
+
 	}
 
 	for _, val := range math["addition"] {
